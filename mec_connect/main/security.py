f8029dd28a50276b1c2c7e027be715db585be12f
Etchegoyen Matthieu <etchegom@gmail.com>
2024-06-07 16:19:45 +0200

    fixing tests

--- mec_connect/main/security.py
+++ mec_connect/main/security.py
@@ -1,7 +1,7 @@
 from __future__ import annotations
 
+import hashlib
 import hmac
-from hashlib import sha256
 from typing import Any
 
 from django.conf import settings
@@ -14,15 +14,16 @@ class SecurityAuth(APIKeyHeader):
     param_name = "Django-Webhook-Signature-v1"
 
     def authenticate(self, request: HttpRequest, key: str | None) -> Any | None:
-        req_timestamp = request.headers.get("Django-Webhook-Request-Timestamp")
+        timestamp = request.headers.get("Django-Webhook-Request-Timestamp")
 
         for signature in key.split(","):
-            digest_payload = bytes(req_timestamp, "utf8") + b":" + request.body
+            digest_payload = bytes(timestamp, "utf8") + b":" + request.body
             digest = hmac.new(
                 key=settings.WEBHOOK_SECRET.encode(),
                 msg=digest_payload,
-                digestmod=sha256,
+                digestmod=hashlib.sha256,
             )
+
             if not hmac.compare_digest(digest.hexdigest(), signature):
                 raise HttpError(401, "Invalid signature")
 
--- mec_connect/main/tests/test_views.py
+++ mec_connect/main/tests/test_views.py
@@ -23,27 +23,24 @@ default_payload = {
     "webhook_uuid": "c209eaf7-8215-4346-9c75-82cf262dc5c3",
 }
 
-headers = {
+default_headers = {
     "Django-Webhook-UUID": "c209eaf7-8215-4346-9c75-82cf262dc5c3",
     "content-type": "application/json",
-    # "django-webhook-signature-v1": "3bb57e3e6d0cdffe967d4557cb20dc7dd0e68a1091806d4a04afed87251c662a",  # noqa: E501
-    # "django-webhook-request-timestamp": "1717761882",
 }
 
 
-def _webhook_headers(payload: dict[str, Any], headers: dict[str, str] = None):
-    now = timezone.now()
-    timestamp = int(datetime.timestamp(now))
-
+def _webhook_headers(payload: dict[str, Any], headers: dict[str, str] = None, secret: str = None):
+    timestamp = int(datetime.timestamp(timezone.now()))
     combined_payload = f"{timestamp}:{json.dumps(payload, cls=JSONEncoder)}"
-
+    secret = secret or settings.WEBHOOK_SECRET
     signature = hmac.new(
-        key=settings.WEBHOOK_SECRET.encode(),
+        key=secret.encode(),
         msg=combined_payload.encode(),
         digestmod=hashlib.sha256,
     ).hexdigest()
 
     return {
+        "Django-Webhook-UUID": "",
         "Django-Webhook-Request-Timestamp": str(timestamp),
         "Django-Webhook-Signature-v1": signature,
     } | (headers or {})
@@ -53,17 +50,18 @@ def _webhook_headers(payload: dict[str, Any], headers: dict[str, str] = None):
 def test_webhook_ok(client):
     resp = client.post(
         reverse("api:webhook"),
-        headers=_webhook_headers(default_payload, headers),
+        headers=_webhook_headers(default_payload, default_headers),
         data=default_payload,
+        content_type="application/json",
     )
     assert resp.status_code == 200, resp.content
 
 
-def test_invalid_signature(client, settings):
-    settings.WEBHOOK_SECRET = "wrong-secret"
+def test_invalid_signature(client):
     resp = client.post(
         reverse("api:webhook"),
-        headers=_webhook_headers(default_payload, headers),
+        headers=_webhook_headers(default_payload, default_headers, secret="wrong-secret"),
         data=default_payload,
+        content_type="application/json",
     )
     assert resp.status_code == 401, resp.content
