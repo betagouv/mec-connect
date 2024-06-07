8949a5f698e306a37e35be5b83f9dccc927f948c
Etchegoyen Matthieu <etchegom@gmail.com>
2024-06-07 14:55:48 +0200

    WIP

--- mec_connect/main/tests/test_views.py
+++ mec_connect/main/tests/test_views.py
@@ -1,9 +1,18 @@
 from __future__ import annotations
 
-import pytest
-from django.urls import reverse
+import hashlib
+import hmac
+import json
+from datetime import datetime
+from json import JSONEncoder
+from typing import Any
 
-payload = {
+import pytest
+from django.conf import settings
+from django.urls import reverse
+from django.utils import timezone
+
+default_payload = {
     "topic": "projects.Project/update",
     "object": {
         "id": 9,
@@ -11,24 +20,50 @@ payload = {
         "updated_on": "2024-02-22T13:38:55.248Z",
     },
     "object_type": "projects.Project",
-    "webhook_uuid": "dcb9e2c6-0781-41fd-bc78-80c5adafb2ea",
+    "webhook_uuid": "c209eaf7-8215-4346-9c75-82cf262dc5c3",
 }
 
 headers = {
-    "django-webhook-uuid": "dcb9e2c6-0781-41fd-bc78-80c5adafb2ea",
-    "Django-Webhook-Signature-v1": "e88c8ba1782769fe0a3060d55313f5b7900df007a9c6980a623a2d7deb40b696",  # noqa: E501
-    "django-webhook-request-timestamp": "1717575565",
+    "Django-Webhook-UUID": "c209eaf7-8215-4346-9c75-82cf262dc5c3",
     "content-type": "application/json",
+    # "django-webhook-signature-v1": "3bb57e3e6d0cdffe967d4557cb20dc7dd0e68a1091806d4a04afed87251c662a",  # noqa: E501
+    # "django-webhook-request-timestamp": "1717761882",
 }
+
+
+def _webhook_headers(payload: dict[str, Any], headers: dict[str, str] = None):
+    now = timezone.now()
+    timestamp = int(datetime.timestamp(now))
+
+    combined_payload = f"{timestamp}:{json.dumps(payload, cls=JSONEncoder)}"
+
+    signature = hmac.new(
+        key=settings.WEBHOOK_SECRET.encode(),
+        msg=combined_payload.encode(),
+        digestmod=hashlib.sha256,
+    ).hexdigest()
+
+    return {
+        "Django-Webhook-Request-Timestamp": str(timestamp),
+        "Django-Webhook-Signature-v1": signature,
+    } | (headers or {})
 
 
 @pytest.mark.django_db
 def test_webhook_ok(client):
-    resp = client.post(reverse("api:webhook"), headers=headers, data=payload)
-    assert resp.status_code == 200
+    resp = client.post(
+        reverse("api:webhook"),
+        headers=_webhook_headers(default_payload, headers),
+        data=default_payload,
+    )
+    assert resp.status_code == 200, resp.content
 
 
 def test_invalid_signature(client, settings):
     settings.WEBHOOK_SECRET = "wrong-secret"
-    resp = client.post(reverse("api:webhook"), headers=headers, data=payload)
-    assert resp.status_code == 401
+    resp = client.post(
+        reverse("api:webhook"),
+        headers=_webhook_headers(default_payload, headers),
+        data=default_payload,
+    )
+    assert resp.status_code == 401, resp.content
