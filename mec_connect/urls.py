dc792330b1a35c5a736fbb3844658ef7d29a4e4d
Etchegoyen Matthieu <etchegom@gmail.com>
2024-06-18 21:54:54 +0200

    add an env indicator on admin title

--- mec_connect/urls.py
+++ mec_connect/urls.py
@@ -1,10 +1,11 @@
 from __future__ import annotations
 
+from django.conf import settings
 from django.contrib import admin
 from django.urls import include, path
 
 admin.site.site_title = "MEC-Connect administration"
-admin.site.site_header = "MEC-Connect administration"
+admin.site.site_header = f"MEC-Connect administration ({settings.ENVIRONMENT})"
 admin.site.index_title = "MEC-Connect administration"
 
 
