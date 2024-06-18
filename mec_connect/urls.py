a23721b11ab0f7481827c4c76fb2ed27080d5601
Etchegoyen Matthieu <etchegom@gmail.com>
2024-06-18 11:04:30 +0200

    customize the admin page

--- mec_connect/urls.py
+++ mec_connect/urls.py
@@ -3,6 +3,11 @@ from __future__ import annotations
 from django.contrib import admin
 from django.urls import include, path
 
+admin.site.site_title = "MEC-Connect administration"
+admin.site.site_header = "MEC-Connect administration"
+admin.site.index_title = "MEC-Connect administration"
+
+
 urlpatterns = [
     path("", include("main.urls")),
     path("admin/", admin.site.urls),
